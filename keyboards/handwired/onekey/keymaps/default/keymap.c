bbd5c355333dcaf8ea6f3fc8717d27333049ade3
dmi3 <dima@dmi3.net>
2023-04-13 21:56:33 +0300

    Scrollwheel

--- keyboards/handwired/onekey/keymaps/default/keymap.c
+++ keyboards/handwired/onekey/keymaps/default/keymap.c
@@ -3,5 +3,14 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    LAYOUT_ortho_1x1(KC_A)
+    LAYOUT_ortho_1x1(KC_BTN3)
 };
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+  if (clockwise) {
+    tap_code(KC_WH_D);
+  } else {
+    tap_code(KC_WH_U);
+  }
+  return true;
+}
